c4076a76063a272795839fad4521c026a15d129c
cbernatfp <cbernatfp@gmail.com>
2023-11-10 17:56:41 +0100

    commit inicial

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into book  values (111, 'Harper Lee', 'Matar a un ruiseñor');
+insert into book  values (222, 'Kent Haruf', 'La nit');
+insert into book  values (333, 'Isabel Allende', 'La casa de los espiritus');
